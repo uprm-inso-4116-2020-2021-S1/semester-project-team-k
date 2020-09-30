178ed9566d80b1ca3b222e411e753b1a91de1234
Sergio R Perez Cuevas <sergio.perez7@upr.edu>
2020-09-30 18:57:32 -0400

    Log In Set Up (WIP)

--- App.js
+++ App.js
@@ -1,21 +1,73 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View, Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.logInContainerStack}>
+        <View style={styles.logInContainer}>
+          <TextInput placeholder = 'Username' style = {styles.logInText} />
+          <TextInput placeholder = 'Password' style = {styles.logInText}/>
+          <Button title = 'Log In'/>
+        </View>
+        <View style={styles.bannerContainerStack}>
+          <View style={styles.bannerContainer}>
+            <View style={styles.logoImage}></View>
+            <Text style={styles.logoText}>
+              Books · School Supplies · School Merch · Clothing
+            </Text>
+          </View>
+          <View style={styles.rect}></View>
+          <View style={styles.rect2}></View>
+        </View>
+      </View>
     </View>
   );
-}
+};
+
+
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex: 1
   },
+  logInContainer: {
+    width: '60%',
+    height: '20%',
+    backgroundColor: "#E6E6E6",
+    marginTop: 400,
+    alignSelf: 'center',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignContent: 'center'
+  },
+  logInText: {
+    padding: 4,
+    borderColor: 'black',
+    borderWidth: 1,
+    margin: 4
+  },
+  bannerContainer: {
+    width: '90%',
+    height: 271,
+    backgroundColor: "#E6E6E6",
+    marginTop: -400,
+    alignSelf: "center"
+  },
+  logoImage: {
+    width: '90%',
+    height: '60%',
+    backgroundColor: "rgba(65,220,90,1)",
+    marginTop: 20,
+    alignSelf: 'center'
+  },
+  logoText: {
+    fontFamily: "roboto-700",
+    color: "#121212",
+    textAlign: "center",
+    fontSize: 13,
+    marginTop: 35,
+    alignSelf: "center"
+  }
 });
