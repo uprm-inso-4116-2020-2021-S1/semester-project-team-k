c30108b0603e1aa61cf56623f4a292d83412e67f
kelvin123459 <kelvin.garcia5@upr.edu>
2020-11-24 13:57:42 -0400

    Changes

--- LogInScreen.js
+++ LogInScreen.js
@@ -1,40 +1,76 @@
 import React from 'react';
-import { StyleSheet, Text, TextInput, View, Button, Image, ImageBackground, IconButton } from 'react-native';
+import { StyleSheet, Text, TextInput, View, Button, Image, ImageBackground, IconButton, AsyncStorage } from 'react-native';
 import { SearchBar } from 'react-native-elements';
 
-function LogInScreen({ navigation }) {
 
-  React.useLayoutEffect(() => {
-    navigation.setOptions({
-      headerTitle: () => (
-        <Image
-        style={{ width: 50, height: 50 }}
-        source={require('./assets/home-icon-01.png')}
-        />
-      ),
-    });
-  }, [navigation]);
-
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <ImageBackground source={require('./assets/fondo-01.png')} style={loginStyles.imglog}>
-      {/* <Text>Home Screen</Text> */}
-
-      {/* <ImageBackground
-        style={{width: 1378/1.5, height: 572/1.5}}
-        source={require('./assets/Logo-01.png')}>
-      </ImageBackground> */}
-
-      <View style={loginStyles.logInContainer}>
-          <TextInput placeholder = 'Username' style = {loginStyles.logInText} />
-          <TextInput placeholder = 'Password' style = {loginStyles.logInText}/>
-          <Button title = 'Log In' style = {loginStyles.logInButton}
-          onPress = {() => navigation.Navigate('Intro') }/>
+export default class Login extends Component{
+  render(){
+    React.useLayoutEffect(() => {
+      navigation.setOptions({
+        headerTitle: () => (
+          <Image
+          style={{ width: 50, height: 50 }}
+          source={require('./assets/home-icon-01.png')}
+          />
+        ),
+      });
+    }, [navigation]);
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+        <ImageBackground source={require('./assets/fondo-01.png')} style={loginStyles.imglog}>
+        {/* <Text>Home Screen</Text> */}
+  
+        {/* <ImageBackground
+          style={{width: 1378/1.5, height: 572/1.5}}
+          source={require('./assets/Logo-01.png')}>
+        </ImageBackground> */}
+  
+        <View style={loginStyles.logInContainer}>
+            <TextInput placeholder = 'email' style = {loginStyles.logInText} onChangeText={(email)=> this.setState({email})} value={this.state.email}/>
+            <TextInput placeholder = 'Password' style = {loginStyles.logInText} onChangeText={(password)=> this.setState({password})} value={this.state.password}/>
+            <Button title = 'Log In' style = {loginStyles.logInButton}
+            onPress = {() => navigation.Navigate('Intro') }/>
+        </View>
+        </ImageBackground>
       </View>
-      </ImageBackground>
-    </View>
-  );
+    );
+  }
+  constructor(props){
+    super(props);
+    this.state={email:'', password:''};
+  }
+
+  login=()=>{
+    fetch('http://HEREGOESCLIENTIP/u', {
+      method: 'POST',
+        headers: {
+          'Accept': 'application/json',
+          'Content Type': 'application/json',
+        },
+          body: JSON.stringify({
+            email: this.state.email,
+            password: this.state.password,
+          })
+    })
+    .then((response) => response.json())
+    .then((res) => {
+      if(res.success === true) {
+        var email = res.message;
+        AsyncStorage.setItem('email', email);
+        this.props.navigator.push({
+
+          //Where is going to be redirected
+          id: 'BrowseScreen'
+        });
+      } else{
+        alert(res.message);
+      }
+    }) 
+    .done();
+  }
 }
+// function LogInScreen({ navigation }) {
+// }
 
 function LogoTitle({ navigation }) {
   return (
@@ -59,7 +95,7 @@ function LogoTitle({ navigation }) {
   );
 }
 
-export default LogInScreen;
+// export default LogInScreen;
 
 const loginStyles = StyleSheet.create({
   logInContainer: {
